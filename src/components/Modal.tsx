708ecc18f443ce464a9f8bd4939c4683e0f1b656
Nikita <ovramenko.nikitka@gmail.com>
2025-01-30 02:24:46 -0500

    added buttons to scroll pictures in the modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,3 +1,5 @@
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+
 interface ModalProps {
   name: string;
   text: JSX.Element | null;
@@ -6,6 +8,20 @@ interface ModalProps {
   closeM: () => void;
   //   pics: string[];
 }
+
+const slideLeft = () => {
+  var slider = document.getElementById("slider2");
+  if (slider) {
+    slider.scrollLeft = slider.scrollLeft - 1000;
+  }
+};
+
+const sliderRight = () => {
+  var slider = document.getElementById("slider2");
+  if (slider) {
+    slider.scrollLeft = slider.scrollLeft + 1000;
+  }
+};
 
 export default function Modal(props: ModalProps) {
   return (
@@ -21,17 +37,37 @@ export default function Modal(props: ModalProps) {
         }}
         className={` fixed  ${
           props.bool ? `opacity-100 scale-100 ` : `opacity-0 scale-50`
-        } duration-300  flex ease-in-out overflow-scroll scroll  flex-col border border-white modal w-4/5 h-5/6 bg-black rounded left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 border-2  `}
+        } duration-300  flex ease-in-out overflow-y-scroll overflow-x-hidden scroll-y flex-col border border-white modal w-4/5 h-5/6 bg-black rounded left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 border-2  `}
       >
-        <div className="  w-full flex h-full items-center  overflow-x-scroll snap-x overflow-y-hidden scroll shrink-0  ">
-          {props.pics.map((pic) => (
-            <img
-              className="  w-full h-auto scale-60 snap-center scale-[0.6]  duration-300 shrink-0"
-              key={pic}
-              src={pic}
-              alt="Image"
-            />
-          ))}
+        <div className="flex items-center justify-center">
+          <MdChevronLeft
+            size={40}
+            className="hover:scale-105 shrink-0 self-center ease-in-out active:scale-50 duration-300 relative "
+            onClick={slideLeft}
+          />
+
+          <div>
+            {" "}
+            <div
+              id="slider2"
+              className="  w-full flex  items-center  overflow-x-scroll scroll-smooth snap-x overflow-y-hidden scroll shrink-0  "
+            >
+              {props.pics.map((pic) => (
+                <img
+                  className="  w-full h-auto scale-60 snap-center scale-[0.6]  duration-300 shrink-0"
+                  key={pic}
+                  src={pic}
+                  alt="Image"
+                />
+              ))}
+            </div>
+          </div>
+
+          <MdChevronRight
+            className="hover:scale-105 shrink-0 self-center ease-in-out active:scale-50 duration-300 relative "
+            size={40}
+            onClick={sliderRight}
+          />
         </div>
 
         {props.text}
