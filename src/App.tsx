013288623f7bb659778a4ec7588275cb09c6cb58
Nikita <ovramenko.nikitka@gmail.com>
2025-02-07 12:36:10 -0500

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,17 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contacts from "./components/Contacts";
 import Footer from "./components/Footer";
+import { useEffect } from "react";
 import "./cssComp/f.css";
 
+import ReactGA from "react-ga4";
+
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-BY5E93F9B3");
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
+
   return (
     <>
       <NavBar />
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import ReactGA from "react-ga4";
-
-ReactGA.initialize("G-BY5E93F9B3");
-
-// Send pageview with a custom path
-ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
